d9d71c88ec20f094df9582d3c417fccebde12816
p3tzata <p3tzata_@abv.bg>
2020-10-15 10:39:06 +0300

    first commit

---
+++ src/main/java/com/example/demo/SpringGuideTrainingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuideTrainingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuideTrainingsApplication.class, args);
+	}
+
+}
